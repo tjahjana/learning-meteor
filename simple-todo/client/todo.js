9f503f845d73dbef84f5b8f2b8d536a91f84f3b2
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2016-10-28 18:53:19 +0200

    remove unusable codes

--- simple-todo/client/todo.js
+++ simple-todo/client/todo.js
@@ -1,36 +1,8 @@
-/*import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});*/
-
 //Create new Mongo collection
 Todos = new Mongo.Collection('todos');
 
-if(Meteor.isClient) {
-	Template.body.helpers ({
-		todos: function() {
-			return Todos.find(); //Display database record in our templates
-		}
-	});
-
-}
-
-if(Meteor.isServer) {
-
-}
+Template.body.helpers ({
+	todos: function() {
+		return Todos.find(); //Display database record in our templates
+	}
+});
