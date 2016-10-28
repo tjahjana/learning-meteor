7557bb8705721707a1ebb884d75ffdce8f4c54dc
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2016-10-28 17:35:01 +0200

    change name to todo

--- simple-todo/client/todo.js
+++ simple-todo/client/todo.js
@@ -1,10 +1,9 @@
-import { Template } from 'meteor/templating';
+/*import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
@@ -16,7 +15,14 @@ Template.hello.helpers({
 
 Template.hello.events({
   'click button'(event, instance) {
-    // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
-});
+});*/
+
+if(Meteor.isClient) {
+
+}
+
+if(Meteor.isServer) {
+	
+}
