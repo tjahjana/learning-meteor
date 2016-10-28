d108738c86f9f46a4bbf98f84cf07a73d14df28b
Asiel Novia Tjahjana <gabriella.asiel@gmail.com>
2016-10-28 19:44:41 +0200

    add input to database and display it using forms

--- simple-todo/client/todo.js
+++ simple-todo/client/todo.js
@@ -3,6 +3,26 @@ Todos = new Mongo.Collection('todos');
 
 Template.body.helpers ({
 	todos: function() {
+		//Access database
 		return Todos.find(); //Display database record in our templates
+	}
+});
+ 
+Template.body.events ({
+	'submit .new-task': function(event) {
+		var title = event.target.title.value;
+
+		//Add record to database
+		Todos.insert ({
+			title: title,
+			createdAt: new Date()
+		});
+
+		//Clear form
+		event.target.title.value = "";
+
+		return false;
+		//If not return false, the page will do refresh because of submit event.
+
 	}
 });
